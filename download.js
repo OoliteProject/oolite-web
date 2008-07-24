fcdfb1cf20c92599a5cdc0d73d5f2778c6e430dd
Jens Ayton <jens@oolite.org>
2008-07-24 13:39:34 +0000

    Check-in of new web site.
    git-svn-id: http://svn.berlios.de/svnroot/repos/oolite-linux/http@1698 127b21dd-08f5-0310-b4b7-95ae10353056

---
+++ download.js
@@ -0,0 +1,146 @@
+var supportedPlatforms = ['mac-os-x', 'windows', 'linux'];
+var platform = identifyPlatform();
+var showAllLink = null;
+
+
+/*	hideOtherPlatforms()
+	Hide <div>s for platforms other than the browser's current platform.
+	If current platform is unknown, does nothing.
+	Currently only run at page load time, but can be safely re-run.
+*/
+function hideOtherPlatforms()
+{
+	// Don't even try it in un-DOMmy browsers
+	if (!supportDOM())  return;
+	
+	// If we can't recognize the platform, show all
+	if (!platform)  return;
+	
+	// Iterate over known platforms
+	for (var i = 0; i < supportedPlatforms.length; i++)
+	{
+		var curr = supportedPlatforms[i];
+		var div = document.getElementById('oolite-dl-' + curr);
+	//	if (!div)  continue;
+		
+		// if ith platform is not the current platform...
+		if (curr != platform)
+		{
+			// ...hide its div.
+			div.style.display = 'none';
+		}
+		else
+		{
+			// This is the current platform; add (or show) "Show all platforms..." link.
+			if (!showAllLink)
+			{
+				div.innerHTML += '<p id="oolite-dl-show-all"><a class="pseudo-control" href="javascript:showAllPlatforms()">Show all platforms...</a></p>';
+				showAllLink = document.getElementById('oolite-dl-show-all');
+				if (!showAllLink && div.children)
+				{
+					showAllLink = div.children.namedItem('oolite-dl-show-all');
+				}
+			}
+			else
+			{
+				showAllLink.style.display = "inline";
+			}
+		}
+	}
+}
+
+
+/*	showAllPlatforms()
+	Show <div>s for all platforms, and hide "show all" link.
+	If current platform is unknown, does nothing.
+*/
+function showAllPlatforms()
+{
+	if (!supportDOM() || !platform)  return;
+	
+	// Show all platforms' divs
+	for (var i = 0; i < supportedPlatforms.length; i++)
+	{
+		var curr = supportedPlatforms[i];
+		var div = document.getElementById('oolite-dl-' + curr);
+		
+		if (div)  div.style.display = 'block';
+	}
+	
+	// Remove "Show all platforms..." link.
+	showAllLink.style.display = "none";
+}
+
+
+function supportDOM()
+{
+	return Boolean(document.getElementById);
+}
+
+
+function identifyPlatform()
+{
+	var result = platformFromPlatformString();
+	if (!result)  result = platformFromUserAgentString();
+	return result;
+}
+
+
+function platformFromPlatformString()
+{
+	// The platform string is the preferred method.
+	var platform = null;
+	if (window.navigator)  platform = window.navigator.platform;
+	if (!platform && window.clientInformation)  platform = window.clientInformation.platform;
+	if (platform)
+	{
+		if (platform.indexOf('Mac') == 0)
+		{
+			return 'mac-os-x';
+		}
+		else if (platform.indexOf('Win') == 0)
+		{
+			return 'windows';
+		}
+		else if ((platform.indexOf('Linux') == 0) || (platform.indexOf('BSD') != 0))
+		{
+			return 'linux';
+		}
+	}
+	return null;
+}
+
+
+function platformFromUserAgentString()
+{
+
+	// No platform string or unknown platform string try sniffing the userAgent (urgh).
+	var userAgent = null;
+	if (window.clientInformation)  userAgent = window.clientInformation.userAgent;
+	if (!userAgent && window.navigator)  userAgent = window.navigator.userAgent;
+	if (userAgent)
+	{
+		if ((userAgent.indexOf('Macintosh') != -1) || (userAgent.indexOf('Mac OS X') != -1))
+		{
+			return 'mac-os-x';
+		}
+		else if ((userAgent.indexOf('Linux') != -1) || (userAgent.indexOf('BSD') != -1))
+		{
+			return 'linux';
+		}
+		else if ((userAgent.indexOf('Windows') != -1) || (userAgent.indexOf('Win32') != -1) || (userAgent.indexOf('Win64') != -1))
+		{
+			return 'windows';
+		}
+	}
+	
+	return null;
+}
+
+
+function printDiagnostics()
+{
+	document.write("platformFromPlatformString(): " + platformFromPlatformString() + "<br>");
+	document.write("platformFromUserAgentString(): " + platformFromUserAgentString() + "<br>");
+	document.write("identifyPlatform(): " + identifyPlatform() + "<br>");
+}
