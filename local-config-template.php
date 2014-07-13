e8d1a87020a59bf1b9c3b049a180c7addb01072a
KonstantinosSykas <getafix@oolite.org>
2014-07-13 23:48:47 +0300

    Added the local-config template

---
+++ local-config-template.php
@@ -0,0 +1,18 @@
+<?php
+
+/* The constants in this file should all be set to the appropriate
+ * values on installation. This file should not be in public version
+ * control! */
+
+// database connection settings
+define("OOLITE_MYSQL_HOST","Oolite add-ons host");
+define("OOLITE_MYSQL_USER","Add ons manager user");
+define("OOLITE_MYSQL_PASS","User password");
+define("OOLITE_MYSQL_DB","Oolite add-ons database sid");
+
+// changing this invalidates all stored passwords
+define("OOLITE_PWHASH","password hash");
+// changing this invalidates current logins
+define("OOLITE_COOKIEHASH","cookie hash");
+
+define("OO_NAV_BASE","");
