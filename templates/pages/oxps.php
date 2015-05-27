c3d01baff74e7627c7ce62504510941a21cc0f9d
KonstantinosSykas <getafix@oolite.org>
2015-05-27 21:52:03 +0300

    Update for Oolite 1.82 release announcement.

--- templates/pages/oxps.php
+++ templates/pages/oxps.php
@@ -1,7 +1,7 @@
 <div id="oxppage">
 
 <p>There are over five hundred expansion packs released for
-Oolite. There are two expansion pack formats in use in Oolite 1.80,
+Oolite. There are two expansion pack formats in use in Oolite 1.80 (or higher),
 usually referred to as OXZ and OXP. The OXZ format is more modern and
 can be downloaded and installed automatically by the game by selecting
 the "Manage Expansion Packs" option on the start screen. The OXP
@@ -11,7 +11,7 @@ currently new, many expansion packs are still only available in the older OXP fo
 <?php include("oxplinks.html"); ?>
 
 <p>The table below lists the OXZ expansion packs which can be
-automatically downloaded by Oolite 1.80, as well as links for manual
+automatically downloaded by Oolite 1.80 (or higher), as well as links for manual
 	 downloads if you prefer to do that. If you are still using Oolite 1.77.1 or an earlier version, OXZ format expansion packs will not be usable - you will be able to download OXP format expansions from other sites.</p>
 
 <?php include("oxptable.php"); ?>
